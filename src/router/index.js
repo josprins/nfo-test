02021331e99b7d31db31b03321f42e066c3fbefc
Jos <j.prins@performation.com>
2026-02-11 19:54:14 +0100

    Added lazy load to pages & spinner on page load

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '@/pages/HomePage.vue'
-import DocsPage from '@/pages/DocsPage.vue'
+const HomePage = () => import('@/pages/HomePage.vue')
+const DocsPage = () => import('@/pages/DocsPage.vue')
 
 const routes = [
 	{ path: '/', component: HomePage },
